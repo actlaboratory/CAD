35b6445fdd75dca0af62c3f1c94909e4e68d2343
yamahubuki <itiro.ishino@gmail.com>
2024-11-04 19:11:41 +0900

    HeaderSettingDialogをKeyValueSettingDialogからひっぺがした

--- views/HeaderSettingDialog.py
+++ views/HeaderSettingDialog.py
@@ -1,41 +1,56 @@
 # -*- coding: utf-8 -*-
 # ヘッダ設定ダイアログ
-#Copyright (C) 2023 yamahubuki <itiro.ishino@gmail.com>
+#Copyright (C) 2023-2024 yamahubuki <itiro.ishino@gmail.com>
 
 import wx
 
-import views.KeyValueSettingDialogBase
-
+from .baseDialog import *
 from entities import Header
 from enumClasses import HeaderFieldType
 from simpleDialog import errorDialog
 
 
-class HeaderSettingDialog(views.KeyValueSettingDialogBase.SettingDialogBase):
+class HeaderSettingDialog(BaseDialog):
 	"""ヘッダの設定内容を入力するダイアログ"""
 
 	def __init__(self, parent, key="", headerType=_("固定値"), body=""):
-		super().__init__(
-			parent,
-			[
-				(_("フィールド名"), True),
-				(_("タイプ"), (_("固定値"), _("編集可能"))),
-				(_("値"), True),
-			],
-			[None]*3,
-			key,headerType,body
-		)
+		super().__init__("valueSettingDialog")
+		self.parent=parent
+		self.values=[key,headerType,body]
 
 	def Initialize(self):
-		return super().Initialize(_("ヘッダ設定"))
+		super().Initialize(self.parent,_("ヘッダ設定"),style=wx.WS_EX_VALIDATE_RECURSIVELY|wx.DEFAULT_FRAME_STYLE)
+		self.InstallControls()
+		return True
+
+	def InstallControls(self):
+		"""いろんなwidgetを設置する。"""
+		self.baseCreator=views.ViewCreator.ViewCreator(self.viewMode,self.panel,self.sizer,wx.VERTICAL,20,style=wx.ALL,margin=20)
+
+		creator=views.ViewCreator.ViewCreator(self.viewMode,self.baseCreator.GetPanel(),self.baseCreator.GetSizer(),wx.HORIZONTAL,10)
+		self.keyEdit,dummy=creator.inputbox(_("フィールド名"),x=500,defaultValue=self.values[0], textLayout=wx.VERTICAL)
+		self.keyEdit.hideScrollBar(wx.HORIZONTAL)
+
+		creator=views.ViewCreator.ViewCreator(self.viewMode,self.baseCreator.GetPanel(),self.baseCreator.GetSizer(),wx.HORIZONTAL,10)
+		state = (_("固定値"), _("編集可能")).index(self.values[1])
+		self.headerTypeCombobox,dummy=creator.combobox(_("タイプ"),(_("固定値"), _("編集可能")),state=state, x=500,textLayout=wx.VERTICAL)
+
+		creator=views.ViewCreator.ViewCreator(self.viewMode,self.baseCreator.GetPanel(),self.baseCreator.GetSizer(),wx.HORIZONTAL,10)
+		self.valueEdit,dummy=creator.inputbox(_("値"),x=500,defaultValue=self.values[2], textLayout=wx.VERTICAL)
+		self.valueEdit.hideScrollBar(wx.HORIZONTAL)
+
+		#ボタンエリア
+		self.creator=views.ViewCreator.ViewCreator(self.viewMode,self.panel,self.sizer,wx.HORIZONTAL,20,"",wx.ALIGN_RIGHT|wx.TOP, 10)
+		self.bOk=self.creator.okbutton(_("ＯＫ"),self.Validation)
+		self.bCancel=self.creator.cancelbutton(_("キャンセル"),None)
 
 	def Validation(self,event):
-		error = Header.validateName(self.edits[0].GetValue())
+		error = Header.validateName(self.keyEdit.GetValue())
 		if error:
 			errorDialog(error, self.wnd)
 			return
 
-		error = Header.validateValue(HeaderFieldType[self.edits[1].GetStringSelection()], self.edits[2].GetValue())
+		error = Header.validateValue(HeaderFieldType[self.headerTypeCombobox.GetStringSelection()], self.valueEdit.GetValue())
 		if error:
 			errorDialog(error, self.wnd)
 			return
@@ -43,8 +58,7 @@ class HeaderSettingDialog(views.KeyValueSettingDialogBase.SettingDialogBase):
 
 	def GetData(self):
 		return [
-			self.edits[0].GetValue().strip(),
-			self.edits[1].GetStringSelection(),
-			self.edits[2].GetValue().strip()
+			self.keyEdit.GetValue().strip(),
+			self.headerTypeCombobox.GetStringSelection(),
+			self.valueEdit.GetValue().strip()
 		]
-
